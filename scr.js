4a53be4b57aea8d4a4b4d0250b119a4f10660cec
aarthi1712 <94539016+aarthi1712@users.noreply.github.com>
2022-09-12 21:35:23 +0530

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,105 +1,75 @@
-function validateAge(today, dobj) {
-  var age = today.getFullYear() - dobj.getFullYear();
-  var mon = today.getMonth() - dobj.getMonth();
-  if (mon < 0 || (mon === 0 && today.getDate() < dobj.getDate())) {
-    age--;
-  }
-  return age;
-}
-let delement = document.getElementById("dob");
-delement.addEventListener("change", () => {
-  let [yy,mm,dd] = document.getElementById("dob").value.split("-");
-  let dob = new Date(yy,mm,dd);
-  let Today = new Date();
-  age = validateAge(Today, dob);
-  if (age < 18 || age > 55) {
-    delement.setCustomValidity("Age must lie in 18 and 55 year`s only");
- 
-    return;
-  } else {
-    delement.setCustomValidity("");
-  }
-});
-let form = document.getElementById("user-form");
+ let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1; //January is 0 so need to add 1 to make it 1!
+        let yyyy = today.getFullYear();
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
+        maxDate = yyyy - 18 + '-' + mm + '-' + dd;
+        minDate = yyyy - 55 + '-' + mm + '-' + dd;
+        document.getElementById("dob").setAttribute("min", minDate);
+        document.getElementById("dob").setAttribute("max", maxDate);
 
-const retriveEntries = () => {
-  let entries = localStorage.getItem("userEntry");
+        let userEntries = localStorage.getItem("user-entries");
+        if (userEntries) {
+            userEntries = JSON.parse(userEntries);
+        } else {
+            userEntries = [];
+        }
 
-  if (entries) {
-    entries = JSON.parse(entries);
-  } else {
-    entries = [];
-  }
-  return entries;
-};
+        const displayEntries = () => {
+            const savedUserEntries = localStorage.getItem("user-entries");
+            let entries = "";
+            if (savedUserEntries) {
+                const parsedUserEntries = JSON.parse(savedUserEntries);
+                entries = parsedUserEntries
+                    .map((entry) => {
+                        const name = `<td class='border px-4 py-2'>${entry.name}</td>`;
+                        const email = `<td class='border px-4 py-2'>${entry.email}</td>`;
+                        const password = `<td class='border px-4 py-2'>${entry.password}</td>`;
+                        const dob = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+                        const acceptTerms = `<td class='border px-4 py-2'>${entry.acceptTermsAndConditions}</td>`;
+                        const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
+                        return row;
+                    })
+                    .join("\n");
+            }
+            var table = `<table class="table-auto w-full"><tr>
+          <th class="px-4 py-2">Name</th>
+          <th class="px-4 py-2">Email</th>
+          <th class="px-4 py-2">Password</th>
+          <th class="px-4 py-2">Dob</th>
+          <th class="px-4 py-2">Accepted terms?</th>
+        </tr>${entries} </table>`;
+            let details = document.getElementById("user-entries");
+            details.innerHTML = table;
+        };
 
-let Entries = retriveEntries();
+        const saveUserForm = (event) => {
+            event.preventDefault();
+            event.preventDefault();
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const dob = document.getElementById("dob").value;
+            const acceptTermsAndConditions =
+                document.getElementById("acceptTerms").checked;
+            const userDetails = {
+                name,
+                email,
+                password,
+                dob,
+                acceptTermsAndConditions,
+            };
+            userEntries.push(userDetails);
+            localStorage.setItem("user-entries", JSON.stringify(userEntries));
 
-const displayEntries = () => {
-  const entries = retriveEntries();
+            displayEntries(); // Add this line
+        };
 
-  const tablerows = entries
-    .map((entry) => {
-      const name = `<td>${entry.name}</td>`;
-      const email = `<td>${entry.email}</td>`;
-      const password = `<td>${entry.password}</td>`;
-      const dob = `<td>${entry.dob}</td>`;
-      const acceptTerms = `<td>${entry.acceptTerms}</td>`;
-
-      const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
-      return row;
-    })
-    .join("\n");
-
-  let tableDiv = document.getElementById("entrytable");
-
-  tableDiv.innerHTML = `<table>
-  <tr>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Password</th>
-    <th>Dob</th>
-    <th>Accepted terms?</th>
-  </tr>
-    ${tablerows}
-  </table>`;
-};
-
-
-const saveUserFrom = (event) => {
-  event.preventDefault();
-
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
-  let dob = document.getElementById("dob").value;
-  let acceptTerms = document.getElementById("acceptTerms").checked;
-
-  let entry_obj = {
-    name,
-    email,
-    password,
-    dob,
-    acceptTerms,
-  };
-
-  Entries.push(entry_obj);
-
-  localStorage.setItem("userEntry", JSON.stringify(Entries));
-
-  displayEntries();
-};
-
-form.addEventListener("submit", saveUserFrom);
-
-displayEntries();
-const email = document.getElementById("email");
-email.addEventListener("input", () => validate(email));
-function validate(ele) {
-  if (ele.validity.typeMismatch) {
-    ele.setCustomValidity("The Email is not in the Right Format..");
-    ele.reportValidity();
-  } else {
-    ele.setCustomValidity("");
-  }
-}
+        let form = document.getElementById("user_form");
+        form.addEventListener("submit", saveUserForm, true);
+        displayEntries();
